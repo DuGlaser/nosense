138ba6f7cfbe2deb5ad817ff531ef3eb488fef4e
DuGlaser <duglaser0618@gmail.com>
2022-07-22 00:44:03 +0900

    :sparkles: `useParseCode`hookを追加

---
+++ web/src/features/editor/hooks/useParseCode.ts
@@ -0,0 +1,25 @@
+import { Lexer, Parser } from '@nosense/damega';
+import { useEffect, useState } from 'react';
+
+import { convert2AstObject } from '@/lib/converter';
+import { AstObject } from '@/lib/models/astObjects';
+
+const parse = (code: string) => {
+  if (code === '') return [];
+
+  const l = new Lexer(code);
+  const p = new Parser(l);
+  const program = p.parseToken();
+  return program.statements.map((_) => convert2AstObject(_));
+};
+
+export const useParseCode = (code: string) => {
+  const [astObjects, setAstObjects] = useState<AstObject[]>(() => parse(code));
+
+  useEffect(() => {
+    if (code === '') return;
+    setAstObjects(parse(code));
+  }, [code]);
+
+  return [astObjects, setAstObjects] as const;
+};
