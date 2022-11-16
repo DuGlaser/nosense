adc9f412bf3ef304ae1b2d3e3b30308be19dc326
DuGlaser <duglaser0618@gmail.com>
2022-11-16 10:56:12 +0900

    :recycle: `CursorNodeComponent`の最大サイズを5pxにする

--- web/src/features/editor/components/CursorNodeComponent/index.tsx
+++ web/src/features/editor/components/CursorNodeComponent/index.tsx
@@ -1,15 +1,22 @@
 import { EditableNodeComponent, EditableNodeProps } from '@editor/components';
+import { styled } from '@mui/material';
+
+const Wrapper = styled('div')({
+  maxWidth: '5px',
+});
 
 export const CursorNodeComponent: React.FC<EditableNodeProps> = (props) => {
   return (
-    <EditableNodeComponent
-      {...props}
-      onKeyDown={(e) => {
-        e.preventDefault();
-      }}
-      onInput={(e) => {
-        e.preventDefault();
-      }}
-    />
+    <Wrapper>
+      <EditableNodeComponent
+        {...props}
+        onKeyDown={(e) => {
+          e.preventDefault();
+        }}
+        onInput={(e) => {
+          e.preventDefault();
+        }}
+      />
+    </Wrapper>
   );
 };
